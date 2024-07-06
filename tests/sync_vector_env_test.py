df658c3ff4841cd6630672e710bf8037d92a9e0e
leoxhwang <1134086740@qq.com>
2024-07-07 01:54:43 +0800

    test: :white_check_mark: pass test

--- tests/sync_vector_env_test.py
+++ tests/sync_vector_env_test.py
@@ -1,25 +1,48 @@
-from smac_pettingzoo import smacv1_pettingzoo_v1
+import sys
+
+from gfootball import gfootball_pettingzoo_v1
+from loguru import logger
+from mamujoco_pettingzoo import mamujoco_pettingzoo_v1
+from smac_pettingzoo import smacv1_pettingzoo_v1, smacv2_pettingzoo_v1
 
 from co_mas.test.sync_vector_env import sync_vector_env_test
 from co_mas.vector import SyncVectorParallelEnv
 
+logger.remove()
+logger.add(sys.stdout, level="INFO")
 
-def env_fn():
+
+def env_smacv1_fn():
     return smacv1_pettingzoo_v1.parallel_env("3m")
 
 
-sync_vec_env = SyncVectorParallelEnv([env_fn for _ in range(2)])
+sync_vec_env = SyncVectorParallelEnv([env_smacv1_fn for _ in range(2)])
 
-# obs, info = sync_vec_env.reset(42)
-# action = {}
+sync_vector_env_test(sync_vec_env, num_cycles=1000)
 
-# for agent in sync_vec_env.possible_agents:
-#     agent_envs = sync_vec_env.envs_have_agent(agent)
-#     if len(agent_envs) > 0:
-#         action[agent] = vector_sample_sample(
-#             agent, obs[agent], info[agent], sync_vec_env.action_space(agent), agent_envs
-#         )
 
-# sync_vec_env.step(action)
+def env_smacv2_fn():
+    return smacv2_pettingzoo_v1.parallel_env("10gen_terran_5_vs_5")
+
+
+sync_vec_env = SyncVectorParallelEnv([env_smacv2_fn for _ in range(2)])
+
+sync_vector_env_test(sync_vec_env, num_cycles=1000)
+
+
+def env_gfootball_fn():
+    return gfootball_pettingzoo_v1.parallel_env("academy_3_vs_1_with_keeper")
+
+
+sync_vec_env = SyncVectorParallelEnv([env_gfootball_fn for _ in range(2)])
+
+sync_vector_env_test(sync_vec_env, num_cycles=1000)
+
+
+def env_mamujoco_fn():
+    return mamujoco_pettingzoo_v1.parallel_env("Ant", "4x2")
+
+
+sync_vec_env = SyncVectorParallelEnv([env_mamujoco_fn for _ in range(2)])
 
 sync_vector_env_test(sync_vec_env, num_cycles=1000)
